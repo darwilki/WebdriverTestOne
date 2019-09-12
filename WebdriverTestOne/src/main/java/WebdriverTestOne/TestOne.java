2c641b81f0f88a9a9c9956bf15505e0ad50b4001
darwilki <darwilki@LUK-5CG90578G5.corp.capgemini.com>
2019-09-12 14:18:55 +0100

    Initial Commit

---
+++ WebdriverTestOne/src/main/java/WebdriverTestOne/TestOne.java
@@ -0,0 +1,45 @@
+package WebdriverTestOne;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.Assert;
+import org.testng.annotations.Test;
+import org.testng.annotations.BeforeTest;	
+import org.testng.annotations.AfterTest;		
+
+public class TestOne {
+	
+private WebDriver driver;
+
+String eTitle = "Meet Guru99 - Free Training Tutorials & Video for IT Courses";
+String aTitle = "";
+
+	@Test
+	public void guru99tutorials() {
+		driver.get("http://www.guru99.com/");
+		aTitle = driver.getTitle();
+		System.out.println(aTitle);
+		
+		if (aTitle.contentEquals(eTitle)) {
+			System.out.println("Test Passed");
+		} else {
+			System.out.println("Test Failed");
+		}
+		
+		Assert.assertTrue(aTitle.equals(eTitle));
+	}
+		
+	@BeforeTest
+	public void beforeTest() {	
+		System.setProperty("webdriver.chrome.driver", "C:\\Users\\darwilki\\Documents\\Selenium\\Downloads\\Drivers\\chromedriver.exe");
+		driver = new ChromeDriver();
+		driver.manage().window().maximize();
+	}	
+		
+		
+	@AfterTest
+	public void afterTest() {
+		driver.quit();			
+	}		
+
+}
---
+++ WebdriverTestOne/src/test/java/WebdriverTestOne/TestOne.java
@@ -0,0 +1,35 @@
+package WebdriverTestOne;
+
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.Assert;
+import org.testng.annotations.Test;
+import org.testng.annotations.BeforeTest;	
+import org.testng.annotations.AfterTest;
+
+public class TestOne {
+
+	private WebDriver driver;
+    
+	@Test				
+	public void testEasy() {	
+		driver.get("http://demo.guru99.com/test/guru99home/");  
+		String title = driver.getTitle();				 
+		Assert.assertTrue(title.contains("Demo Guru99 Page")); 		
+	}	
+	
+	
+	@BeforeTest
+	public void beforeTest() {	
+		System.setProperty("webdriver.chrome.driver", "C:\\Users\\darwilki\\Documents\\Selenium\\Downloads\\Drivers\\chromedriver.exe");
+	    driver = new ChromeDriver();
+	    driver.manage().window().maximize();
+	}		
+	
+	
+	@AfterTest
+	public void afterTest() {
+		driver.quit();			
+	}		
+}
